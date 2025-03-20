8be53e8ff059a51dd59e58d3341b6f71bebc9922
Dubey <manoj.dubey@kroger.com>
2025-03-19 23:15:53 -0400

    save me

--- mybashscripts/02-shenbang.sh
+++ mybashscripts/02-shenbang.sh
@@ -6,3 +6,5 @@ sleep 3
 echo "Using bash we will be automating the manual effort and this will save time"
 sleep 3 
 echo "Script Name: shenbank.sh completed"
+sleep 3
+echo "Thank you for your time and have a good day"
