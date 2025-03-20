eb6d3c54c085ea29e1facfcbc14f45f78fc36b94
Dubey <manoj.dubey@kroger.com>
2025-03-19 23:14:43 -0400

    save me

--- mybashscripts/02-shenbang.sh
+++ mybashscripts/02-shenbang.sh
@@ -5,4 +5,4 @@ echo "Today is my first session of bash scripting by Neha only to learn"
 sleep 3
 echo "Using bash we will be automating the manual effort and this will save time"
 sleep 3 
-echo "Script Name: "shenbank.sh" completed"
+echo "Script Name: shenbank.sh completed"
