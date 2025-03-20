82b9c850c4de1be41223e5e3607ff561d0038598
Dubey <manoj.dubey@kroger.com>
2025-03-19 22:53:24 -0400

    save me

--- mybashscripts/01-print.sh
+++ mybashscripts/01-print.sh
@@ -3,4 +3,10 @@ echo "Hello World"
 echo "This is my first bash script"
 echo "I am learning bash scripting"
 
+# -e flag is used to enable interpretation of backslash escapes
+echo -e "Hello World\nThis is my first bash script\nI am learning bash scripting"
 
+# -e is used for special characters like \n, \t, \b, \r, \f, \v, \a, \\, \', \"
+echo -e "Hello World\tThis is my first bash script\nI am learning bash scripting"
+
+echo -e "Hello\n\MyName is Manoj\n\How you doing?"
