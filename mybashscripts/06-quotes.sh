7f7982bb8a789598fa406958b7aaef6f56f3029d
Dubey <manoj.dubey@kroger.com>
2025-03-21 15:30:14 -0400

    auto update

--- mybashscripts/06-quotes.sh
+++ mybashscripts/06-quotes.sh
@@ -3,10 +3,10 @@
 # ""   : Double Quotes 
 # ''   : Single Quotes   : # Single Quotes Always Eliminates The Power Of The Special Variable 
 
-a=100 
-echo "Demo on quotes:"
-echo $?
-echo '$?'
-echo "Value of is $?" 
+a=1050
+echo "Demo on this quotes:"
+#echo $?
+#echo '$?'
+#echo "Value of is $?" 
 echo "Value of a is $a"
-echo 'Value of a is $a'
+#echo 'Value of a is $a'
