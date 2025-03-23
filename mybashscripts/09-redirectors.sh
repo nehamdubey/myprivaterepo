f8f6a41c14ced83441aeee3382926090429dc52b
Dubey <manoj.dubey@kroger.com>
2025-03-22 22:34:30 -0400

    auto update

--- mybashscripts/09-redirectors.sh
+++ mybashscripts/09-redirectors.sh
@@ -8,11 +8,11 @@
 #     2) Output Redirector  ( >> or > )( Means routing the output to a file )        :    > or 1>  or >>  or  &>  ( >> appends the latest output to the existing content)  
 
     # Outputs 
-    #   1) Standard Output                          1> or > or >> ( Append )
-    #   2) Standard Error                           2> or 2>> 
-    #   3) Standard Output and Standard Error       &> or &>> 
+        1) Standard Output                          1> or > or >> ( Append )
+        2) Standard Error                           2> or 2>> 
+        3) Standard Output and Standard Error       &> or &>> 
 
- ls -ltr    >  output.txt   # Redirects the output to  output.txt
+# ls -ltr    >  output.txt   # Redirects the output to  output.txt
 # ls -ltr    >> output.txt   # Redirects and appends the output to  output.txt
 # ls -ltr    2> output.txt   # Redirects the error only to  output.txt
 # ls -ltr    &> output.txt   # Redirects the output or error output.txt
